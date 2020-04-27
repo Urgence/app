47a0662a727088851700d801defa02755a497d4a
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-04-27 16:17:58 +0200

    add carousel

--- src/views/HomeScreen.tsx
+++ src/views/HomeScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import { Subheading, Title } from 'react-native-paper';
+import styled from 'styled-components';
+
+const Titles = styled(Title)`
+
+`;
 
 
 export default function HomeScreen() {
 
     return (
         <SafeAreaView>
-            <Text>Open up App.tsx to start working on your app!</Text>
+                <Titles>Autour de vous ye ...</Titles>
+            <Subheading>Les Etablissements </Subheading>
         </SafeAreaView>
     );
 }
