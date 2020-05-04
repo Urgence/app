714dcca41e1c334b354fa70bd676497e404e3ffa
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-05-04 13:51:12 +0200

    add data place info

--- src/views/PlaceInfoScreen.tsx
+++ src/views/PlaceInfoScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, ImageBackground, Dimensions, Text, Linking, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, ImageBackground, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { Button } from 'react-native-paper';
 import dialCall from '../utils/disCallServices';
@@ -37,21 +37,24 @@ const styles = StyleSheet.create({
 });
 
 
-
-export default function PlaceInfo() {
-
+export default function PlaceInfo({ route, navigation }) {
+    const [data, setData] = useState();
+    useEffect(() => {
+        console.log('QUERY', route.params.query);
+    }, []);
     return (
         <SafeAreaView>
-            <ImageBackground style={ styles.imgBackground }
-                 source={{uri: 'https://www.lobservateur.fr/wp-content/uploads/2020/04/Capture-d%E2%80%99e%CC%81cran-2020-04-15-a%CC%80-20.54.43.png'}}>
-                    <Text style={ styles.info } >Test</Text>
-                    <Button style={ styles.maps } mode="contained" onPress={() => dialCall(15) }>
-                        J'y vais
-                    </Button>
-                    <Button style={ styles.call } mode="contained" onPress={() => console.log('Pressed')}>
-                        Appeler
-                    </Button>
-            </ImageBackground>
+            {/*<ImageBackground style={styles.imgBackground}*/}
+            {/*                 // source={{ uri: 'https://www.lobservateur.fr/wp-content/uploads/2020/04/Capture-d%E2%80%99e%CC%81cran-2020-04-15-a%CC%80-20.54.43.png' }}*/}
+            {/*>*/}
+                <Text style={styles.info}>Test</Text>
+                <Button style={styles.maps} mode="contained" onPress={() => dialCall(15)}>
+                    J'y vais
+                </Button>
+                <Button style={styles.call} mode="contained" onPress={() => console.log('Pressed')}>
+                    Appeler
+                </Button>
+            {/*</ImageBackground>*/}
         </SafeAreaView>
     );
 }
